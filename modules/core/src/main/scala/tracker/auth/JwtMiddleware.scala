fb292241bde24f7ec883ec01f7c33ce03ec1bbfd
satish <satish.manit@gmail.com>
2023-02-11 22:25:28 +0530

    fix jwt middleware

--- modules/core/src/main/scala/tracker/auth/JwtMiddleware.scala
+++ modules/core/src/main/scala/tracker/auth/JwtMiddleware.scala
@@ -1,29 +1,32 @@
 package tracker.auth
 
+import cats.MonadThrow
 import cats.data.{Kleisli, OptionT}
 import org.http4s.{AuthedRequest, Request, Response, Status}
 import org.http4s.server.AuthMiddleware
 import org.typelevel.ci.CIString
+import cats.syntax.all.*
 
 object JwtMiddleware:
 
-  def authUser[F[_], U](token: Token[F,U]): Kleisli[OptionT[F,_], Request[F], U] = Kleisli {
-    req =>
-      authHeaderValue(req).map {
-        case Some(t) => token.verify(t)
-        case None => None
-      }
-  }
+  private def authUser[F[_] : MonadThrow, U](token: Token[F, U]): Kleisli[OptionT[F,_], Request[F], U] =
+    val header : Kleisli[OptionT[F,_], Request[F], String] = Kleisli {
+      req => OptionT(authHeaderValue(req).pure[F])
+    }
+    val userFunc: Kleisli[OptionT[F,_], String, U] = Kleisli {
+      s => OptionT(token.verify(s).map(Some(_)).handleErrorWith(_ => None.pure[F]))
+    }
+    header.andThen(userFunc)
 
-  def authHeaderValue[F[_]](req: Request[F]): Option[String] =
+  private def authHeaderValue[F[_]](req: Request[F]): Option[String] =
     req.headers.get(CIString("Authorization")).map {
       ls => ls.head.value
     }
 
-  def middleware[F[_], U](authUser: Kleisli[OptionT[F,_], Request[F], U]): AuthMiddleware[F, U] =
+  def middleware[F[_]: MonadThrow, U](authUser: Kleisli[OptionT[F,_], Request[F], U]): AuthMiddleware[F, U] =
     service => Kleisli {
       req =>
-        OptionT(userAuth(req).value.flatMap {
+        OptionT(authUser(req).value.flatMap {
           case Some(a) => service(AuthedRequest(a, req)).value
           case None => Some(Response(Status.Unauthorized)).pure[F]
         })
